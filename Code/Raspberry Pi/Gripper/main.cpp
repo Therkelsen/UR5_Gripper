3bf9df0cf05be52eedb4f3b433ce5d57e1f6c390
Therkelsen <thomastherkelsen191200@gmail.com>
2021-04-27 10:19:18 +0200

    Wrote some basic boilerplate code for motor control - Will probably be replaced soon

--- Code/Raspberry Pi/Gripper/main.cpp
+++ Code/Raspberry Pi/Gripper/main.cpp
@@ -6,12 +6,37 @@
 #include<unistd.h>
 #endif
 #include <wiringPi.h>
+#include <softPwm.h>
 
 #include <xmlrpc-c/base.hpp>
 #include <xmlrpc-c/registry.hpp>
 #include <xmlrpc-c/server_abyss.hpp>
 
+#define motorEnable 1
+#define motorPin1 0
+#define motorPin2 2
+
+using namespace std;
+
 int main() {
+    cout << "Inclusions and definitions handled" << endl;
+
+    wiringPiSetup();
+
+    pinMode(motorPin1,OUTPUT);
+    pinMode(motorPin2,OUTPUT);
+    pinMode(motorEnable,OUTPUT);
+
+    softPwmCreate(motorEnable,0,100);
+    cout << "GPIO access has been set up" << endl;
+
+    softPwmWrite(motorEnable,HIGH);
+    digitalWrite(motorPin1,HIGH);
+    digitalWrite(motorPin2,LOW);
+
+    delay(5000);
+
+    softPwmWrite(motorEnable,LOW);
 
     return 0;
 }
