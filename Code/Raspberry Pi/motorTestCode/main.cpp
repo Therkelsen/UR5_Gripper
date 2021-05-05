1b1bdb8eaaecef486d4779601e58792d3f924f1e
Therkelsen <37107686+Therkelsen@users.noreply.github.com>
2021-05-05 14:13:18 +0200

    Refactoring code to put motor control in its own class, so code looks more clean.
    Co-Authored-By: alexellegaard <73937005+alexellegaard@users.noreply.github.com>

--- Code/Raspberry Pi/motorTestCode/main.cpp
+++ Code/Raspberry Pi/motorTestCode/main.cpp
@@ -1,33 +1,19 @@
 
 #include <iostream>
-#include <wiringPi.h>
-#include <softPwm.h>
+#include "motorcontrol.h"
 
 #define motorEnable 1
 #define motorPin1 0
 #define motorPin2 2
 
+int beerCan = 1000;
+
 using namespace std;
 
 int main() {
-    cout << "Inclusions and definitions handled" << endl;
+    MotorControl mc(motorEnable, motorPin1, motorPin2);
 
-    wiringPiSetup();
-
-    pinMode(motorPin1,OUTPUT);
-    pinMode(motorPin2,OUTPUT);
-    pinMode(motorEnable,OUTPUT);
-
-    softPwmCreate(motorEnable,0,100);
-    cout << "GPIO access has been set up" << endl;
-
-    cout << "Driving motor with 100% power for 5 seconds" << endl;
-    softPwmWrite(motorEnable,100);
-
-    delay(5000);
-
-    cout << "Stopping motor" << endl;
-    softPwmStop(motorEnable);
+    mc.grip(beerCan);
 
     return 0;
 }
--- Code/Raspberry Pi/motorTestCode/motorcontrol.h
+++ Code/Raspberry Pi/motorTestCode/motorcontrol.h
@@ -2,10 +2,17 @@
 #define MOTORCONTROL_H
 
 
-class motorControl
+class MotorControl
 {
 public:
-    motorControl();
+    MotorControl();
+    MotorControl(int motorEnable, int motorPin1, int motorPin2);
+    void calibrate();
+    void grip(int object);
+    void release();
+
+private:
+    int _motorEnable, _motorPin1, _motorPin2;
 };
 
 #endif // MOTORCONTROL_H
